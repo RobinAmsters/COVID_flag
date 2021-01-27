e715c2b3a0c9b7dc38f3c9051dd6988f1596e1e5
Robin Amsters <robin.amsters@kuleuven.be>
2021-01-27 16:25:44 +0100

    get cumulative cases per day

--- vis.py
+++ vis.py
@@ -2,18 +2,28 @@
 # @brief 
 # @author Robin Amsters
 # @bug No known bugs
+
+# Data source: https://epistat.wiv-isp.be/covid/
+
 import matplotlib.pyplot as plt
 import pandas as pd
 
 # Load data
-df = pd.read_excel('data/COVID19BE.xlsx', sheet_name="CASES_AGESEX", index_col="DATE")
-print(df)
+data = pd.read_excel('data/COVID19BE.xlsx', sheet_name="CASES_AGESEX", index_col="DATE")
+
 # Format data
-df = df.dropna() # Drop final nan entries
-df = df.groupby(["DATE"]).sum() # Get cumulative number of cases, groups municipality, age and sex together
+data = data.dropna() # Drop final nan entries
+data = data.groupby(["DATE"]).sum() # Get total number of cases per day, groups municipality, age and sex together
+df_cum = data["CASES"].cumsum() # Get cumulative number of cases per day
 
 # Visualization
-df.plot()
-plt.ylabel("Cases per day")
-plt.grid()
+fig, axs = plt.subplots(2)
+data.plot(ax=axs[0], legend=False)
+axs[0].set_ylabel("Cases per day")
+axs[0].grid()
+
+df_cum.plot(ax = axs[1])
+axs[1].set_ylabel("Cumulative cases")
+axs[1].grid()
+
 plt.show()
